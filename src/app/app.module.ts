270a3de3a70c7d613985edb4d62dad62a63e7d54
Cristian Cazacu <vladislav.tornea@gmail.com>
2019-03-05 02:14:55 +0200

    example to add personalized custom error messages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,23 @@ import { MatButtonModule } from '@angular/material';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-// tslint:disable-next-line: no-implicit-dependencies
-import { NgFormsModule } from '@kryshac/ng-forms';
+import { ERROR_MESSAGES_TOKEN, ErrorMessage, NgFormsModule } from '@kryshac/ng-forms';
 
 import { AppComponent } from './app.component';
+
+export const errorMessages: Partial<ErrorMessage> = {
+  required: 'test error message',
+};
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, BrowserAnimationsModule, FormsModule, ReactiveFormsModule, MatButtonModule, NgFormsModule],
-  providers: [],
+  providers: [
+    {
+      provide: ERROR_MESSAGES_TOKEN,
+      useValue: errorMessages,
+    },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
